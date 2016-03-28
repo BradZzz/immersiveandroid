860583ccd6755fcb690bc60d72b42f991cde59a7
Mauve3 <mauvemoonman@gmail.com>
2016-03-28 11:04:42 -0700

    commit for save

--- assets/js/controllers/partials/HeaderCtrl.js
+++ assets/js/controllers/partials/HeaderCtrl.js
@@ -27,7 +27,7 @@ function ($scope, $state, $timeout, $mdSidenav, $log, seQuotes, seUser)
     },
     test : seUser.user,
     nav : [
-        { icon : 'ion-android-home', text : 'Home', click : function(){console.log("clicked home")} },
+        { icon : 'ion-android-home', text : 'Home', click : function(){$state.go('home')} },
         { icon : 'ion-folder', text : 'Profile', click : function(){$state.go('profile')} },
         { icon : 'ion-ios-pulse-strong', text : 'Analytics', click : function(){console.log("clicked analytics")} },
         { icon : 'ion-ios-gear', text : 'Settings', click : function(){console.log("clicked settings")} },
