5d3403650170118344dcd374ef57282077d04e04
Mauve3 <mauvemoonman@gmail.com>
2016-03-30 03:25:42 -0700

    added new favicon

--- assets/js/services/se-authorization.js
+++ assets/js/services/se-authorization.js
@@ -8,9 +8,12 @@ function ($rootScope, $state, sePrincipal)
         .then(function() {
           var isAuthenticated = sePrincipal.isAuthenticated()
           if ('data' in $rootScope.toState && 'role' in $rootScope.toState.data && !sePrincipal.isInRole($rootScope.toState.data.role)) {
+            console.log('bad')
             if (isAuthenticated) {
+              console.log('bad2')
               $state.go('home') // user is signed in but not authorized for desired state
             } else {
+              console.log('bad3')
               // user is not authenticated. stow the state they wanted before you
               // send them to the signin state, so you can return them when you're done
               $rootScope.returnToState = $rootScope.toState
--- assets/js/services/se-theme.js
+++ assets/js/services/se-theme.js
@@ -8,7 +8,9 @@ function ($http)
   self.backgrounds = ['assets/img/backgrounds/wall_1.png','assets/img/backgrounds/wall_2.png',
       'assets/img/backgrounds/wall_3.png','assets/img/backgrounds/wall_4.png','assets/img/backgrounds/wall_5.png',
       'assets/img/backgrounds/wall_6.png','assets/img/backgrounds/wall_7.png','assets/img/backgrounds/wall_8.png',
-      'assets/img/backgrounds/wall_9.png','assets/img/backgrounds/wall_10.png','assets/img/backgrounds/wall_11.png']
+      'assets/img/backgrounds/wall_9.png','assets/img/backgrounds/wall_10.png','assets/img/backgrounds/wall_11.png',
+      'assets/img/backgrounds/wall_12.png','assets/img/backgrounds/wall_13.png','assets/img/backgrounds/wall_14.png',
+      'assets/img/backgrounds/wall_15.png','assets/img/backgrounds/wall_16.png','assets/img/backgrounds/wall_17.png']
 
   self.print = function (response) {
     console.log(self.logName + " response: ", response)
