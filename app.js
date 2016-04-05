1da6449047bd22bddb273fca3cce0d2098065a69
Mauve3 <mauvemoonman@gmail.com>
2016-04-04 23:26:18 -0700

    styling

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ var templateConfig = {
   extname: '.html',
 }
 
-if (app.get('prod')) {
+//if (app.get('prod')) {
   templateConfig.partialsDir = path.join(__dirname, 'dist')
-} else {
-  templateConfig.partialsDir = __dirname
-}
+//} else {
+//  templateConfig.partialsDir = __dirname
+//}
 
 app.engine('html', exphbs(templateConfig))
 app.set('view engine', 'html')
