02cd26a7d7d4aa57f341990b2dce6402c1f63d87
Mauve3 <mauvemoonman@gmail.com>
2016-06-21 13:13:44 -0700

    somethingm

--- assets/js/controllers/partials/HeaderCtrl.js
+++ assets/js/controllers/partials/HeaderCtrl.js
@@ -80,6 +80,9 @@ function ($scope, $state, $rootScope, $timeout, $mdSidenav, $log, seMedia, seThe
         { icon : 'ion-folder', text : 'Profile', click : function(){$state.go('profile')} },
         { icon : 'ion-ios-pulse-strong', text : 'Analytics', click : function(){$state.go('analytics')} },
         { icon : 'ion-ios-gear', text : 'Settings', click : function(){$state.go('settings')} },
+        { icon : 'ion-android-contacts', text : 'Feedback', click : function(){ $window.location.href =
+            "https://docs.google.com/a/generalassemb.ly/forms/d/1ARX9BR3SV-60kOjAHZ0Sah3PBC9khGUmtD70i4iogYU/viewform" }
+        },
         { icon : 'ion-android-exit', text : 'Logout', click : function(){ $scope.loginRegister.logout() } },
     ],
     incBackground : function (offset) {
